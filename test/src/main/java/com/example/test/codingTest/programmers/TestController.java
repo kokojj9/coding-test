062b6e527580053ce309134a64e9a6749c1b18d5
kokojj <kovjscl976@naver.com>
2024-08-26 01:24:39 +0900

    8/26 문제풀이

--- test/src/main/java/com/example/test/codingTest/programmers/TestController.java
+++ test/src/main/java/com/example/test/codingTest/programmers/TestController.java
@@ -1,14 +1,17 @@
 package com.example.test.codingTest.programmers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TestController {
 
     // n보다 커질때 까지 (리스트,배열)
     public int solution1(int[] numbers, int n) {
         int answer = 0;
 
-        for(int num: numbers){
+        for (int num : numbers) {
             answer += num;
-            if(answer > n) break;
+            if (answer > n) break;
         }
 
         return answer;
@@ -21,7 +24,7 @@ public class TestController {
 
         int index = 0;
 
-        for(int i = 0; i < num_list.length; i += n){
+        for (int i = 0; i < num_list.length; i += n) {
             answer[index++] = num_list[i];
         }
 
@@ -32,9 +35,7 @@ public class TestController {
     public int[] solution3(int[] num_list, int n) {
         int[] answer = new int[n];
 
-        for(int i = 0; i < n; i++){
-            answer[i] = num_list[i];
-        }
+        System.arraycopy(num_list, 0, answer, 0, n);
 
         return answer;
     }
@@ -44,13 +45,9 @@ public class TestController {
         int length = num_list.length;
         int[] answer = new int[length];
 
-        for(int i = 0; i < length - n; i++){
-            answer[i] = num_list[n + i];
-        }
+        if (length - n >= 0) System.arraycopy(num_list, n + 0, answer, 0, length - n);
 
-        for(int i = 0; i < n; i++){
-            answer[length - n + i] = num_list[i];
-        }
+        if (n >= 0) System.arraycopy(num_list, 0, answer, length - n + 0, n);
 
         return answer;
         // 순환배열 개념사용
@@ -63,7 +60,7 @@ public class TestController {
         int[] answer = new int[length - n + 1];
         int idx = 0;
 
-        for(int i = n - 1; i < length; i++){
+        for (int i = n - 1; i < length; i++) {
             answer[idx++] = num_list[i];
         }
 
@@ -74,9 +71,11 @@ public class TestController {
 
     // 첫 번째로 나오는 음수
     public int solution6(int[] num_list) {
-        for(int i = 0; i < num_list.length; i++){
-            if(num_list[i] < 0) return i;
+        for (int i = 0; i < num_list.length; i++) {
+            if (num_list[i] < 0) return i;
         }
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.toArray();
 
         return -1;
     }
@@ -87,22 +86,62 @@ public class TestController {
         int[] answer = new int[gap + 1];
         int index = 0;
 
-        for(int i = 0; i <= gap; i++){
+        for (int i = 0; i <= gap; i++) {
             answer[index++] = start_num - i;
         }
 
         return answer;
     }
+
+    // 배열 만들기1
+    public int[] solution8(int n, int k) {
+        int[] answer = new int[n / k];
+        int index = 0;
+
+        for (int i = k; i <= n; i += k) {
+            answer[index++] = i;
+        }
+        String str = "banana";
+        System.out.println(str.indexOf("an"));
+
+        return answer;
+    }
+
+    // 문자열 섞기
+    public String solution9(String str1, String str2) {
+        StringBuilder answer = new StringBuilder();
+
+        for (int i = 0; i < str1.length(); i++) {
+            answer.append(str1.charAt(i)).append(str2.charAt(i));
+        }
+
+        return answer.toString();
+    }
+
+    // 리스트 문자열로 전환
+    public String solution10(String[] arr) {
+        return String.join("", arr);
+    }
+
+    // 문자열 곱하기
+    public String solution11(String my_string, int k) {
+        return my_string.repeat(k);
+    }
+
+    // 더 크게 합치기
+    public int solution12(int a, int b) {
+        String num1 = Integer.toString(a) + b;
+        String num2 = Integer.toString(b) + a;
+
+        return num1.compareTo(num2) >= 0 ? Integer.parseInt(num1) : Integer.parseInt(num2);
+        
+        //return Math.max(Integer.parseInt(a + "" + b), Integer.parseInt(b + "" + a)); 다른사람 풀이
+    }
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    // 두 수의 연산값 비교하기
+    public int solution(int a, int b) {
+        return Math.max(Integer.parseInt(a + "" + b), (2 * a * b));
+    }
+
+
 }
